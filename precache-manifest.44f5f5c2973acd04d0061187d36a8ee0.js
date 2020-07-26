cc1071d1b8018f43a9c3f906b9b827f02f89b431
Lars Kliesing <lgk@live.de>
2020-07-26 16:11:43 +0200

    Updates

---
+++ precache-manifest.44f5f5c2973acd04d0061187d36a8ee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d69b04f378ff0f2d56b332227fe81a",
+    "url": "/navimunch-ui/index.html"
+  },
+  {
+    "revision": "81ee515895c4c6e0400e",
+    "url": "/navimunch-ui/static/css/main.c2ab1a4b.chunk.css"
+  },
+  {
+    "revision": "977623a828c23f26c9a3",
+    "url": "/navimunch-ui/static/js/2.48ff455b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/navimunch-ui/static/js/2.48ff455b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81ee515895c4c6e0400e",
+    "url": "/navimunch-ui/static/js/main.4607afb9.chunk.js"
+  },
+  {
+    "revision": "350a06e0a99f82b58e12",
+    "url": "/navimunch-ui/static/js/runtime-main.1f814253.js"
+  }
+]);
